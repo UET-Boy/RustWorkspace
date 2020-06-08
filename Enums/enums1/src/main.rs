ed09452d2ddc885258f84c349531295deb8e90d6
UET-Boy <Spellbreaker907@gmail.com>
2020-06-08 18:28:05 +0500

    second commit

--- Enums/enums1/src/main.rs
+++ Enums/enums1/src/main.rs
@@ -3,10 +3,12 @@ fn main() {
     let student1 = Student::Public(String::from("Hamza"));
     let student2 = Student::Private(String::from("Faisal"));
     let student3 = Student::Overseas(String::from("Imran"));
+    let fee = Student::FeeSlab(20, 40, 80);
 
     println!("{:?}", student1);
     println!("{:?}", student2);
     println!("{:?}", student3);
+    println!("{:?}", fee);
 }
 
 #[derive(Debug)]
@@ -15,4 +17,5 @@ enum Student{
     Private(String),
     Public(String),
     Overseas(String),
+    FeeSlab(u8,u8,u8),
 }
